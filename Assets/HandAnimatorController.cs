965ace7543850a4e2102476ebe1e2e77167ef54b
NVL VUC <nvl@vucfyn.net>
2024-08-02 13:29:51 +0200

    Added Hands & Animation
    Dog virker den ene animation ikke

---
+++ Assets/HandAnimatorController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HandAnimatorController : MonoBehaviour
+{
+    [SerializeField] private InputActionProperty triggerAction;
+    [SerializeField] private InputActionProperty gripAction;
+
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        float triggerValue = triggerAction.action.ReadValue<float>();
+        float gripValue = triggerAction.action.ReadValue<float>();
+
+        animator.SetFloat("Trigger", triggerValue);
+        animator.SetFloat("Grip", gripValue);
+    }
+}
